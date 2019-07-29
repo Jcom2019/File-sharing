b2425d2f7442f3162a2635bdaea2c49948c44ba1
Joas Japhes <Joasjaphes665@gmail.com>
2019-07-29 13:47:08 +0300

    Update login_nav.blade.php

--- resources/views/layouts/login_nav.blade.php
+++ resources/views/layouts/login_nav.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>File Sharing</title>
+    <title>File||Sharing</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
